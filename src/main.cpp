d2a8fe602267877a8d222d03745c8d2a6cd0456f
lnikon <bejanyan.vahag@pm.me>
2022-12-20 23:00:09 +0400

    Add: Integrate conan & Boost

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,18 @@
+#include <boost/system/detail/error_code.hpp>
 #include <iostream>
 
+#include <boost/asio.hpp>
+#include <boost/system/error_code.hpp>
+
 int main() {
-    std::cout << "NetworkMonitor is live!" << std::endl;
+    boost::system::error_code ec {};
+    if (ec) {
+        std::cerr << "Error: " << ec.message() << std::endl;
+        return -1;
+    } else {
+        std::cout << "OK" << std::endl;
+        return 0;
+    }
 
     return 0;
 }
