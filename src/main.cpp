fe58c3f7bf89f62ed37a6da2baa0d7a2f7336a64
lnikon <bejanyan.vahag@pm.me>
2022-12-20 21:45:06 +0400

    Add: Project skeleton

---
+++ src/main.cpp
@@ -0,0 +1,7 @@
+#include <iostream>
+
+int main() {
+    std::cout << "NetworkMonitor is live!" << std::endl;
+
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <utility>
+
+int main() {
+    std::pair<std::string, double> pair {"Hello, World!", 42};
+
+    const auto& [str, num] = pair;
+    std::cout << str << ": " << num << std::endl;
+
+    return 0;
+}
